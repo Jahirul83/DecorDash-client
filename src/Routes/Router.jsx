665e939987f11ec54e61b708b6ec427e1ebf0015
Jahirul83 <jahirulislamcse83@gmail.com>
2024-06-06 14:13:51 +0600

    router setup

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../layout/Root";
+import ErrorPage from "./ErrorPage";
+import Home from "../Pages/Home/Home/Home";
+
+
+const Router =createBrowserRouter([
+    {
+      path: "/",
+      element: <Root></Root>,
+      errorElement: <ErrorPage></ErrorPage>,
+      children:[
+        {
+            path:"/",
+            element: <Home></Home>
+        },
+    ]
+    },
+  ]);
+
+export default Router;
